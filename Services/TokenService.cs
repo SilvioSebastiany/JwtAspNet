089da95c762a64765cc637bbb332a5c7120e46f1
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2025-04-30 14:05:03 -0300

    Assinando o Token

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,4 +1,6 @@
 using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JwtAspNet.Services;
 
@@ -7,6 +9,13 @@ public class TokenService
     public string Create()
     {
         var tokenHandler = new JwtSecurityTokenHandler();
+
+        var key = Encoding.ASCII.GetBytes(Configuration.PrivateKey);
+
+        new SigningCredentials(
+            new SymmetricSecurityKey(key),
+            SecurityAlgorithms.HmacSha256
+        );
 
         var token = tokenHandler.CreateToken(); 
         var tokenString = tokenHandler.WriteToken(token);
