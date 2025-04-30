265131dcd867da98bd7e67ab19df426fff9b2d83
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2025-04-30 12:49:18 -0300

    Criando o arquivo de configuração

--- Configuration.cs
+++ Configuration.cs
@@ -2,6 +2,5 @@ namespace JwtAspNet;
 
 public static class Configuration
 {
-    public static string PrivateKey { get; set; } = "1sdfgs"; // 32 bytes
-
+    public static string PrivateKey { get; set; } = "q3Jv9X1+2kL8yZ5hT7w4n8p6r1Jv3X2y5Z8t6w9kL3p"; 
 }
