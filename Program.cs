124c6b3d4a203ea404a1b77b2de360e1f9a7ce04
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2025-04-30 14:28:06 -0300

    Gerando o Token

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using JwtAspNet.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Adiciona o serviço de TokenService ao contêiner de injeção de dependência
+// Isso permite que o TokenService seja injetado em controladores ou outros serviços
+builder.Services.AddTransient<TokenService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configura o pipeline de requisições HTTP
+// Define um endpoint GET na raiz ("/") que cria e retorna um token JWT
+app.MapGet("/", (TokenService tokenService) =>
+{
+    var token = tokenService.Create();
+    return Results.Ok(token);
+});
 
 app.Run();
--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -8,16 +8,27 @@ public class TokenService
 {
     public string Create()
     {
+        // Cria um token JWT
         var tokenHandler = new JwtSecurityTokenHandler();
 
+        // transforma a chave privada em bytes
         var key = Encoding.ASCII.GetBytes(Configuration.PrivateKey);
 
-        new SigningCredentials(
+
+        // Cria as credenciais de assinatura do token
+        var credentials = new SigningCredentials(
             new SymmetricSecurityKey(key),
             SecurityAlgorithms.HmacSha256
         );
 
-        var token = tokenHandler.CreateToken(); 
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            SigningCredentials = credentials,           // Chave privada para assinar o token
+            Expires = DateTime.UtcNow.AddHours(2),      // Tempo de expiração do token
+        };
+
+
+        var token = tokenHandler.CreateToken(tokenDescriptor); 
         var tokenString = tokenHandler.WriteToken(token);
         return tokenString;
     }
