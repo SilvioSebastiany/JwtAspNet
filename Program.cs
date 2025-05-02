dc4dedd6b7311ec0c854d552d83bca44db14e94b
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2025-05-02 16:30:42 -0300

    Obtendo Claims do Token

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using System.Text;
 using JwtAspNet;
 using JwtAspNet.Models;
@@ -24,7 +25,11 @@ builder.Services.AddAuthentication(x =>
         ValidateAudience = false, // Não valida o público do token (pode ser ajustado conforme necessário)
     };
 });  
-builder.Services.AddAuthorization();  
+builder.Services.AddAuthorization(x => 
+{
+    // Define uma política de autorização chamada "admin" que requer o papel "Admin"
+    x.AddPolicy("admin", b => b.RequireRole("Admin")); 
+}); 
 
 
 var app = builder.Build();
@@ -35,8 +40,9 @@ app.UseAuthentication();
 app.UseAuthorization();   
 
 // Configura o pipeline de requisições HTTP
-// Define um endpoint GET na raiz ("/") que cria e retorna um token JWT
-app.MapGet("/", (TokenService tokenService) =>
+// Define o endpoint de login que gera um token JWT para o usuário
+// O endpoint é acessível sem autenticação, pois é onde o token é gerado
+app.MapGet("/login", (TokenService tokenService) =>
 {
     var user = new User( 
         1, 
@@ -44,12 +50,25 @@ app.MapGet("/", (TokenService tokenService) =>
         "silvio.sebastiany@gmail.com", 
         "https://avatars.githubusercontent.com/u/1021230?v=4",
         "123456", 
-        new string[] { "Admin", "User" }
+        new string[] { "User" }
     ); 
 
     // Chama o método Create do TokenService para gerar um token JWT para o usuário
     return tokenService.Create(user);
 
 });
+
+app.MapGet("/restrito",(ClaimsPrincipal user) => new
+{
+    id = user.Claims.FirstOrDefault(x => x.Type == "id")?.Value,
+    name = user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Name)?.Value,
+    email = user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value,
+    giveName = user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.GivenName)?.Value,
+    image = user.Claims.FirstOrDefault(x => x.Type == "image")?.Value, 
+}).RequireAuthorization();
+
+app.MapGet("/admin",() => "You tem acesso")
+    .RequireAuthorization("admin"); // Define um endpoint protegido que requer autenticação e o papel "admin"
+
 
 app.Run();
